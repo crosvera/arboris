fc397104ca8d7b6d9a11dd22895bda8b8a3443db
Carlos Rios Vera <crosvera@gmail.com>
2014-01-09 00:42:10 -0300

    guess.py: fixed. training.py: testing the training of the ANN

--- guess.py
+++ guess.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     ann = libfann.neural_net()
     ann.create_from_file(sys.argv[1])
     
-    ori = cv2.imread(fimg, cv2.IMREAD_COLOR)
+    ori = cv2.imread(sys.argv[2], cv2.IMREAD_COLOR)
 
     img = ori.copy()
     img = bgr2gray(img)
@@ -22,6 +22,7 @@ if __name__ == "__main__":
     f3 = get_leaf_vein_features(ori.copy(), cnt)
     f4 = get_convex_hull_feature(cnt)
 
-    f = list(f1+f2+f3).append(f4)
+    #f = list(f1+f2+f3).append(f4)
+    f = f1[0], f1[1], f1[2], f1[3], f1[4], f1[5], f1[6], f1[7], f1[8], f1[9], f2[0], f2[1], f2[2], f2[3], f2[4], f3[0], f3[1], f3[2], f3[3], f3[4], f4
 
     print ann.run(f)
--- training.py
+++ training.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     connection_rate = 1
     learning_rate = 0.7
     num_input = 20
-    num_hidden = 4
+    num_hidden = 3
     num_output = 1
 
     desired_error = 0.0001
@@ -17,7 +17,8 @@ if __name__ == "__main__":
     ann = libfann.neural_net()
     ann.create_sparse_array(connection_rate, (num_input, num_hidden, num_output))
     ann.set_learning_rate(learning_rate)
-    ann.set_activation_function_output(libfann.SIGMOID_SYMMETRIC_STEPWISE)
+    ann.set_activation_function_hidden(libfann.ELLIOT_SYMMETRIC)
+    ann.set_activation_function_output(libfann.ELLIOT_SYMMETRIC)
 
     ann.train_on_file(sys.argv[1], max_iterations, iterations_between_reports, desired_error)
 
